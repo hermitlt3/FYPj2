dac634ddf358b3469c3d0939bb52d874a6b0d864
TEDDY\Terence <mingjie097@gmail.com>
2017-06-22 01:39:48 +0800

    For now just change some textures in all canvas, testing on scripting event system

---
+++ Assets/MainMenuControls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MainMenuControls : MonoBehaviour {
+
+    public EventSystem System;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.name = "Continue Button";
+        System.firstSelectedGameObject = gameObject;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
