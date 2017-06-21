eae594fcf83bfe712b13e74d8f578ba3ee4f4de6
potatolol <unknownpotato@live.com>
2017-06-21 15:50:13 +0800

    Added an empty game object called the game area whereby if the player falls out of its area he dies! Fixed the collectibles deactivate all bug and player camera reset when player dies. And some other animatios and prefabs shenanigans.

---
+++ Assets/Scripts/Checkpoints/GameAreaScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAreaScript : MonoBehaviour {
+
+	void Start () {
+
+	}
+
+	void OnTriggerExit2D(Collider2D colli) 
+	{
+		if (colli.transform.gameObject.tag == "Player") {
+			colli.transform.gameObject.GetComponent<Stat_HealthScript> ().DecreaseHealth (colli.gameObject.GetComponent<Stat_HealthScript> ().GetCurrentHealth ());
+		}
+	}
+}
