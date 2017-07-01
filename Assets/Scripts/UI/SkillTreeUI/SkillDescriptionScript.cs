2baae7dd5d5ac3384628f8c01a9745245ef47118
potatolol <unknownpotato@live.com>
2017-07-02 03:34:12 +0800

    Time to do the player feedback for skill tree. By tmr, cursor, audio, cutscenes.

---
+++ Assets/Scripts/UI/SkillTreeUI/SkillDescriptionScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillDescriptionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void ShowDescription(RectTransform transform, int flipX, int flipY) {
+		flipX = Mathf.Clamp (flipX, -1, 1);
+		flipY = Mathf.Clamp (flipY, -1, 1);
+
+		if (flipX == 0) {
+			flipX = 1;
+		}
+		if (flipY == 0) {
+			flipY = 1;
+		}
+
+		Vector3 targetPosition;
+		targetPosition = new Vector3(
+			(transform.rect.width * 1.5f  + transform.GetChild(0).GetComponent<RectTransform>().rect.width ) * flipX, 
+			-(transform.rect.height * 0.5f + transform.GetChild(0).GetComponent<RectTransform>().rect.height * flipY)  
+		);
+		this.transform.position = transform.position + targetPosition;
+		this.gameObject.SetActive (true);
+	}
+
+	public void HideDescription() {
+		this.gameObject.SetActive (false);
+	}
+}
---
+++ Assets/Scripts/UI/SkillTreeUI/SkillUIScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SkillUIScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	public GameObject description;
+
+	public int flipX;
+	public int flipY;
+
+	private RectTransform rectTransform;
+	// Use this for initialization
+	void Start () {
+		rectTransform = GetComponent<RectTransform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnPointerEnter(PointerEventData eventData) {
+		description.GetComponent<SkillDescriptionScript> ().ShowDescription (rectTransform, flipX, flipY);
+	}
+
+	public void OnPointerExit(PointerEventData eventData) {
+		description.GetComponent<SkillDescriptionScript> ().HideDescription ();
+	}
+}
