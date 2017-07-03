e5f22326465151a7561a6eacba132720015c21bd
TEDDY\Terence <mingjie097@gmail.com>
2017-07-03 11:12:11 +0800

    Need reset boss position after testing. now can load into another scene after boss is dead

---
+++ Assets/Scripts/Resetables/ResetPlayerLocation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetPlayerLocation : MonoBehaviour {
+
+    public Vector2 Level1;
+    public Vector2 Level2;
+
+    public GameObject Player;
+
+    bool SceneChanged;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameObject.Find("FollowCamera2D").GetComponent<LevelTransition>().isTriggered)
+        {
+            ResetLocation();
+            GameObject.Find("FollowCamera2D").GetComponent<LevelTransition>().isTriggered = false;
+        }
+	}
+
+    public void ResetLocation()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "Real")
+            Player.transform.position = Level1;
+        if (scene.name == "Level 2")
+            Player.transform.localPosition = Level2;
+    }
+
+
+}
---
+++ Assets/Scripts/Transition/LevelTransition.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour {
+
+    public static LevelTransition instance;
+
+
+    public Stat_HealthScript Boss;
+    public string MapName;
+    public bool isTriggered;
+
+    private float TimeDelay = 0.0f;
+
+    void Start()
+    {
+        isTriggered = false;
+        TimeDelay = 0.0f;
+    }
+
+    void Update()
+    {
+        if (!Boss.isAlive() && !isTriggered)
+        {
+            TimeDelay++;
+        }
+        if (TimeDelay >= 200.0f && !isTriggered)
+        {
+            isTriggered = true;
+            RunOnce();
+            TimeDelay = 0.0f;
+        }
+    }
+
+    void RunOnce()
+    {
+        StartCoroutine(SceneTransitManager.instance.ChangeScene(MapName));
+    }
+}
