6e1e797b187a1291c22acc60bc824eda3e5988fd
potatolol <unknownpotato@live.com>
2017-07-13 19:55:53 +0800

    Tutorials somewhat done. Left decorations and transiting from one scene to another and custom events. Todolater. Still needfix camera reset position not velocity-ing.

---
+++ Assets/Scripts/Events/TutorialEndEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialEndEvent : CustomEventBaseScript
+{
+    public GameObject boss;
+	// Use this for initialization
+	protected override void Start () {
+		
+	}
+
+    // Update is called once per frame
+    protected override void Update () {
+		if(!boss.GetComponent<Stat_HealthScript>().isAlive() && !startEvent)
+        {
+            startEvent = true;
+            StartCoroutine(SceneTransitManager.instance.FadeInAndOut());
+        }
+	}
+}
---
+++ Assets/Scripts/Objectives/InteractiveObjective/InteractiveObjectiveScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractiveObjectiveScript : BaseObjectiveScript
+{
+    public float animSpeed = 0.5f;
+    Animator thisAnimator;
+    public float displayFor = 2f;
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        base.Start();
+        description = "Interactibles trigger events.\n(Look or listen for them)";
+
+        GetComponent<Text>().text = description;
+
+        thisAnimator = GetComponent<Animator>();
+        thisAnimator.SetFloat(Animator.StringToHash("FadeSpeed"), animSpeed);
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+        displayFor = Mathf.Max(0, displayFor - Time.deltaTime);
+        if (displayFor <= 0f)
+        {
+            doneObjective = true;
+            thisAnimator.SetBool(Animator.StringToHash("FadeOut"), doneObjective);
+        }
+    }
+
+    public override void DestroyItself()
+    {
+        Destroy(this.gameObject);
+    }
+}
