4a82e29b3e7c31e3b7db1a6f6449e0a362a19989
Terence <mingjie097@gmail.com>
2017-07-14 10:41:52 +0800

    Added Mixer into main menu, fixed background for option and about. Rearranged level 2 terrain

---
+++ Assets/Scripts/Audio/AudioScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioMixer masterMixer;
+
+    public void SetSound(float soundLevel)
+    {
+        masterMixer.SetFloat("MusicVol", soundLevel);
+    }
+}
