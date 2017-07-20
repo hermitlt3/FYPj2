c2c0e3c083a70400dd3eebe7a8319ec9a75c99d3
potatolol <unknownpotato@live.com>
2017-07-20 16:26:07 +0800

    Some tutorial shenaniganssss

---
+++ Assets/Scripts/Events/TutorialEvents/PreTutorialEventStart.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PreTutorialEventStart : CustomEventBaseScript {
+
+    public GameObject player;
+    public UITypewriterScript typeWriter;
+    public GameObject[] toBeActivated;
+    public GameObject[] toBeDestroyed;
+    public GameObject fade;
+
+    public Text theText;
+
+    Animator animator;
+    bool fadeText;
+    bool fadeBlack;
+
+    // Use this for initialization
+    protected override void Start () {
+        if(player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").gameObject;
+        }
+        player.GetComponent<Player_Input>().enabled = false;
+        animator = GetComponent<Animator>();
+        animator.SetBool("Start", true);
+	}
+
+    // Update is called once per frame
+    protected override void Update () {
+		if(fadeText && theText.color.a > 0)
+        {
+            theText.color -= new Color(0, 0, 0, Time.deltaTime);
+        }
+        if(fadeBlack && fade.GetComponent<Image>().color.a > 0)
+        {
+            fade.GetComponent<Image>().color -= new Color(0, 0, 0, Time.deltaTime);
+        }
+    }
+    
+    void StartTyping(int letters)
+    {
+        StartCoroutine(typeWriter.RunText(letters));
+    }
+
+    void ReverseTyping(int hi)
+    {
+        if(hi > 0)
+        {
+            StartCoroutine(typeWriter.RemoveAllTextTimely());
+        }
+        else
+        {
+            typeWriter.BackspaceText();
+        }
+    }
+
+    void ChangeText(string newText)
+    {
+        typeWriter.ChangeText(newText);
+    }
+
+    void ChangeTextColor(string color)
+    {
+        if(color == "Red")
+        {
+            theText.color = Color.red;
+        }
+    }
+
+    void SetTypeWriterTime(float time)
+    {
+        typeWriter.delayTime = time;
+    }
+
+    void FadeWords()
+    {
+        fadeText = true;
+    }
+
+    void FadeBlack()
+    {
+        fadeBlack = true;
+    }
+
+    // The bool to say its over
+    void SetFinish()
+    {
+        isDone = true;
+        Destroy(typeWriter);
+        player.GetComponent<Player_Input>().enabled = true;
+        foreach (GameObject go in toBeActivated)
+        {
+            go.SetActive(true);
+        }
+        foreach (GameObject go in toBeDestroyed)
+        {
+            Destroy(go);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/EffectsUI/UITypewriterScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITypewriterScript : MonoBehaviour
+{
+    Text theText;
+    string story;
+
+    public float delayTime = 0.125f;
+
+    void Start()
+    {
+        theText = GetComponent<Text>();
+        story = theText.text;
+        theText.text = "";
+    }
+
+    public IEnumerator RunText(int numberOfLetters)
+    {
+        int temp = 0;
+        foreach (char c in story)
+        {
+            temp++;
+            if (temp > numberOfLetters)
+            {
+                break;
+            }
+
+            theText.text += c;
+            yield return new WaitForSeconds(delayTime);
+        }
+    }
+
+    public void BackspaceText()
+    {
+        if(theText.text.Length > 0)
+        {
+            theText.text = theText.text.Remove(theText.text.Length - 1);
+        }
+    }
+
+    public IEnumerator RemoveAllTextTimely()
+    {
+        while(theText.text.Length > 0)
+        {
+            theText.text = theText.text.Remove(theText.text.Length - 1);
+            yield return new WaitForSeconds(delayTime);
+        }
+    }
+
+    public void ChangeText(string newText)
+    {
+        story = newText;
+    }
+}
