5560e5d63d4114922f82328f1af2dc6ba5a4a239
Terence <mingjie097@gmail.com>
2017-07-24 18:04:34 +0800

    Increase Background image for gameplay, fix mainmenu background as well. Do a button on screen to notify ppl of skill level

---
+++ Assets/Scripts/UI/SkillTreeUI/SkillLevelPopup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillLevelPopup : MonoBehaviour {
+
+    public GameObject UI;
+
+    void Update()
+    {
+        if (GameObject.FindGameObjectWithTag("Player").GetComponent<Stat_AbilityPoint>().GetAbilityPoint() > 0)
+        {
+            UI.SetActive(true);
+        }
+        else
+        {
+            UI.SetActive(false);
+        }
+    }
+
+}
