63f6f78f484379329d267f0c3a19a64caa484874
potatolol <unknownpotato@live.com>
2017-07-29 21:47:32 +0800

    Moved some files into their respective folders. Created sound when click on UI

---
+++ Assets/Scripts/UI/GameUI/PlaySoundOnClickScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlaySoundOnClickScript : MonoBehaviour, IPointerDownHandler{
+
+    public AudioSource onCickSound;
+
+    // Use this for initialization
+    void Start () {
+		if(onCickSound == null)
+        {
+            print("No sound on click");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        onCickSound.Play();
+    }
+}
