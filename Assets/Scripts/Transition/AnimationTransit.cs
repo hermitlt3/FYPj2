8d16b9549a4de8e4a12dd7e34275ab62b6d4f0d4
Terence <mingjie097@gmail.com>
2017-07-30 17:09:38 +0800

    A simple scripted Credit Scene

---
+++ Assets/Scripts/Transition/AnimationTransit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AnimationTransit : MonoBehaviour {
+
+    public Animator Clip;
+
+    public string MapName;
+	// Use this for initialization
+	void Start () {
+        Clip.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AnimatorStateInfo StateInfo = Clip.GetCurrentAnimatorStateInfo(0);
+        if (StateInfo.IsName("End"))
+            StartCoroutine(SceneTransitManager.instance.ChangeScene(MapName));
+
+	}
+}
